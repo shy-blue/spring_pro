c320f3979676ef265a4c567da0970a19e9145a2f
肖蔚 <weilan@qq.com>
2021-08-10 16:55:04 +0800

    新增ABC的类

---
+++ 01-spring/src/main/java/com/weilan/entity/ABC.java
@@ -0,0 +1,12 @@
+package com.weilan.entity;
+
+/**
+ * @author weilan
+ * @create 2021-08-10 16:53
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println(11111);
+        System.out.println(2222);
+    }
+}
